66b82309364b4da71b7543862f9d25e75c58c398
Felipe Maciel <160450207+engfrontfelipe@users.noreply.github.com>
2025-06-03 08:59:21 -0300

    abrindo porta do servidor

--- Backend/service.cjs
+++ Backend/service.cjs
@@ -71,3 +71,8 @@ app.post('/generate-pdf', async (req, res) => {
   }
 });
 
+const PORT = process.env.PORT || 3333;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
