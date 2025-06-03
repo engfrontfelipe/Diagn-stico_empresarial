64e888841154b861615f8f9396cebba4a6257ef1
Felipe Maciel <160450207+engfrontfelipe@users.noreply.github.com>
2025-06-03 09:30:18 -0300

    corrigindo import.

--- Backend/service.cjs
+++ Backend/service.cjs
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const puppeteer = require("puppeteer");
-const chromium = require('chromium');
+const chromium = require("chrome-aws-lambda");
 const app = express();
 
 const { generateHtml } = require('./src/routes/pdfGenerate.cjs');
